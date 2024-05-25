4b80ba2a57bc4af0d6b4abe1d187c6a033c93500
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-05-25 18:45:31 +0530

    minor

--- src/static/sampleData/users.ts
+++ src/static/sampleData/users.ts
@@ -1,68 +1,74 @@
-export const users = [
-  {
-    userId: 1,
-    name: "John Doe",
-    age: 30,
-    address: "123 Main St, Somewhere USA"
-  },
-  {
-    userId: 2,
-    name: "Jane Smith",
-    age: 25,
-    address: "456 Oak Rd, Somewhere City"
-  },
-  {
-    userId: 3,
-    name: "Bob Johnson",
-    age: 42,
-    address: "789 Elm St, Elsewhere Town"
-  },
-  {
-    userId: 4,
-    name: "Alice Williams",
-    age: 35,
-    address: "321 Maple Ave, Somewhere Else"
-  },
-  {
-    userId: 5,
-    name: "Tom Davis",
-    age: 28,
-    address: "159 Oak St, Another Town"
-  },
-  {
-    userId: 6,
-    name: "Sara Lee",
-    age: 31,
-    address: "753 Pine Rd, Somewhere Else"
-  },
-  {
-    userId: 7,
-    name: "Michael Brown",
-    age: 45,
-    address: "951 Elm Blvd, Elsewhere City"
-  },
-  {
-    userId: 8,
-    name: "Emily Wilson",
-    age: 27,
-    address: "357 Oak Dr, Somewhere Else"
-  },
-  {
-    userId: 9,
-    name: "David Anderson",
-    age: 39,
-    address: "852 Maple St, Another Place"
-  },
-  {
-    userId: 10,
-    name: "Olivia Thompson",
-    age: 33,
-    address: "654 Pine Ave, Somewhere New"
-  },
-  {
-    userId: 11,
-    name: "William Garcia",
-    age: 29,
-    address: "258 Elm Rd, Elsewhere Town"
-  }
+interface User {
+	userId: number;
+	name: string;
+	age: number;
+	address: string;
+}
+export const users: User[] = [
+	{
+		userId: 1,
+		name: "John Doe",
+		age: 30,
+		address: "123 Main St, Somewhere USA",
+	},
+	{
+		userId: 2,
+		name: "Jane Smith",
+		age: 25,
+		address: "456 Oak Rd, Somewhere City",
+	},
+	{
+		userId: 3,
+		name: "Bob Johnson",
+		age: 42,
+		address: "789 Elm St, Elsewhere Town",
+	},
+	{
+		userId: 4,
+		name: "Alice Williams",
+		age: 35,
+		address: "321 Maple Ave, Somewhere Else",
+	},
+	{
+		userId: 5,
+		name: "Tom Davis",
+		age: 28,
+		address: "159 Oak St, Another Town",
+	},
+	{
+		userId: 6,
+		name: "Sara Lee",
+		age: 31,
+		address: "753 Pine Rd, Somewhere Else",
+	},
+	{
+		userId: 7,
+		name: "Michael Brown",
+		age: 45,
+		address: "951 Elm Blvd, Elsewhere City",
+	},
+	{
+		userId: 8,
+		name: "Emily Wilson",
+		age: 27,
+		address: "357 Oak Dr, Somewhere Else",
+	},
+	{
+		userId: 9,
+		name: "David Anderson",
+		age: 39,
+		address: "852 Maple St, Another Place",
+	},
+	{
+		userId: 10,
+		name: "Olivia Thompson",
+		age: 33,
+		address: "654 Pine Ave, Somewhere New",
+	},
+	{
+		userId: 11,
+		name: "William Garcia",
+		age: 29,
+		address: "258 Elm Rd, Elsewhere Town",
+	},
 ];
